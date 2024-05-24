f3ccb79be4199c87f664c6ecc49f57ec121fda1d
Lukkas98 <lucaspalma988@gmail.com>
2024-05-24 15:41:15 -0300

    fix text

--- components/category.jsx
+++ components/category.jsx
@@ -2,6 +2,7 @@ import InputSearch from "./(header)/(search)/inputSearch";
 import ProductCard from "./productCard";
 
 export default function Category({ products, searchParams }) {
+  const { search } = searchParams;
   return (
     <section className="min-h-[80vh] max-h-full min-w-screen z-10">
       <div className="h-full overflow-hidden overflow-y-auto scrollbar-thin lg:grid">
@@ -15,7 +16,9 @@ export default function Category({ products, searchParams }) {
         {products.length <= 0 && (
           <div className=" flex justify-center mt-10 py-3 rounded-xl text-white bg-red-500 w-[85%] mx-auto">
             <p className=" font-semibold text-lg text-center">
-              Ups... No encontramos lo que andabas buscando
+              {search
+                ? `Ups... no encontramos nada con ${search}`
+                : "Ups... No encontramos nada por aquí"}
             </p>
           </div>
         )}
