d04af86fa39ecec9b9be4236b0f506f8545d456c
Lukkas98 <lucaspalma988@gmail.com>
2024-10-30 10:14:44 -0300

    fix styles

--- app/admin/panel/components/tabs/paymentsTab.jsx
+++ app/admin/panel/components/tabs/paymentsTab.jsx
@@ -16,20 +16,22 @@ export default function PaymentsTab({ data }) {
                   className={`${
                     payment.approved
                       ? "bg-green-800 hover:bg-green-900 text-gray-100"
-                      : "bg-blue-800 hover:bg-blue-900 text-white"
-                  } px-3 py-4 max-w-xl mx-5 rounded-lg shadow-lg transition-all flex justify-between items-center`}
+                      : "bg-blue-900 hover:bg-blue-950 text-white"
+                  } px-3 py-2 max-w-xl mx-5 rounded-lg shadow-md shadow-black transition-all flex justify-between items-center`}
                   key={i}
                 >
                   <div className="mb-3">
-                    <p className="font-semibold text-lg">{payment.id}</p>
-                    <p className="text-sm">Productos: {payment.items.length}</p>
+                    <p className="font-semibold text-base whitespace-nowrap">
+                      {payment.id}
+                    </p>
+                    <p className="text-xs">Productos: {payment.items.length}</p>
                     <p className="text-sm mt-2">
                       Total:{" "}
-                      <span className="font-semibold text-base">
+                      <span className="font-semibold text-base text-green-400">
                         ${payment.total}
                       </span>
                     </p>
-                    <p className="text-gray-300 mt-1">
+                    <p className="text-gray-300 text-sm mt-2">
                       {new Date(payment.updatedAt).toLocaleString("es-AR", {
                         year: "numeric",
                         month: "2-digit",
@@ -39,8 +41,8 @@ export default function PaymentsTab({ data }) {
                       })}
                     </p>
                     {payment.approved && (
-                      <span className="text-xs text-gray-200 font-medium italic mt-2 block">
-                        Se eliminará automáticamente después de 7 días
+                      <span className="text-xs whitespace-nowrap text-gray-200 font-medium italic mt-2 block">
+                        Se eliminará automáticamente en 7 días
                       </span>
                     )}
                   </div>
