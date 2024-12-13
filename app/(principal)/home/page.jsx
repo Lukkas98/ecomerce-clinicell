1cf260bdb1d59bae0ca12278ecbf70500d785ecf
Lukkas98 <lucaspalma988@gmail.com>
2024-12-13 15:50:06 -0300

    add HomePage

---
+++ app/(principal)/home/page.jsx
@@ -0,0 +1,7 @@
+export default async function HomePage() {
+  return (
+    <div className="bg-gray-900 text-gray-100 flex min-h-[320px] flex-col justify-between lg:justify-stretch lg:grid max-w-[2000px] mx-auto">
+      <h1>Home</h1>
+    </div>
+  );
+}
