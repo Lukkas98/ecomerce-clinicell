363022903843d47f8d8a683465d2738db2d5e617
Lukkas98 <lucaspalma988@gmail.com>
2025-01-03 15:22:53 -0300

    refactor OutletSection component; replace product display with ProductOutlet component and add links for more visibility

--- app/(principal)/home/components/outletSection.jsx
+++ app/(principal)/home/components/outletSection.jsx
@@ -1,39 +1,32 @@
+import ProductOutlet from "@/components/outlet/productOutlet";
 import { ProductModel } from "@/models/product";
-import { CheckBadgeIcon } from "@heroicons/react/24/outline";
-import Image from "next/image";
+import Link from "next/link";
 
 export default async function OutletSection() {
   const products = await ProductModel.find({ outlet: true, stock: true }).limit(
     3
   );
 
+  if (products.length === 0) return null;
+
   return (
     <section className="w-full bg-gradient-to-r from-red-600 to-orange-500 p-6 rounded-lg shadow-lg text-center">
       <h2 className="text-3xl font-semibold mb-2">
-        <span className="animate-pulse">🔥</span> ¡Productos en Outlet!{" "}
+        <span className="animate-pulse">🔥</span> ¡Productos en{" "}
+        <Link href={"/outlet"} className="text-orange-900">
+          Outlet!
+        </Link>{" "}
         <span className="animate-pulse">🔥</span>
       </h2>
       <p className="text-lg">
-        Descuentos imperdibles en productos seleccionados.
+        Descuentos imperdibles en productos seleccionados.{" "}
+        <Link href={"/outlet"} className="underline text-sm">
+          Ver Más
+        </Link>
       </p>
       <div className="flex gap-4 justify-center mt-4">
         {products.map((product) => (
-          <div
-            key={product._id}
-            className="w-32 h-fit p-2 bg-gray-800 rounded-md shadow-md flex gap-2 flex-col justify-center items-center"
-          >
-            <p className="line-clamp-2">{product.name}</p>
-            <Image
-              src={product.images[0]}
-              width={100}
-              height={100}
-              alt="imag"
-              className="rounded-md"
-            />
-            <p className=" text-green-500 flex gap-2 items-center font-semibold">
-              $ {product.price} <CheckBadgeIcon width={20} height={20} />
-            </p>
-          </div>
+          <ProductOutlet key={product._id} product={product} />
         ))}
       </div>
     </section>
