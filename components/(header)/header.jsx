12f398676157eefb42e73e37f873923de0d588f0
Lukkas98 <lucaspalma988@gmail.com>
2025-01-07 14:26:11 -0300

    refactor Navbar component; remove commented code for cleaner structure and readability

--- components/(header)/header.jsx
+++ components/(header)/header.jsx
@@ -13,7 +13,6 @@ export default function Navbar() {
   return (
     <header className="bg-gray-900 text-white sticky top-0 z-50 w-full shadow-sm shadow-gray-950">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        {/* Logo e información */}
         <div className="flex items-center gap-3">
           <Image
             src="/logo.png"
@@ -31,14 +30,12 @@ export default function Navbar() {
           </div>
         </div>
 
-        {/* Barra de búsqueda */}
         <div className="flex-1 mx-4 hidden md:block">
           <InputSearch className="w-full px-4 py-2 rounded-md bg-gray-800 border border-gray-700 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
         </div>
 
         {/* Carrito y menú */}
         <div className="flex items-center gap-6">
-          {/* Menú hamburguesa para mobile */}
           <button className="sm:hidden" onClick={() => setMenuOpen(!menuOpen)}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -56,7 +53,6 @@ export default function Navbar() {
             </svg>
           </button>
 
-          {/* Botones del menú para tablet y desktop */}
           <nav className="hidden sm:flex gap-4">
             <Link href={"/home"} className="text-sm hover:text-blue-500">
               Home
@@ -67,8 +63,6 @@ export default function Navbar() {
             {/* <button className="text-sm hover:text-blue-500">Ofertas</button> */}
             <MegaMenu />
           </nav>
-
-          {/* Carrito */}
           <button className="relative">
             <Cart />
           </button>
