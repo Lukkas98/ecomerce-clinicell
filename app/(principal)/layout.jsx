e386611128a8ee5d610eaccdcc0be1a4888a6816
Lukkas98 <lucaspalma988@gmail.com>
2025-01-14 12:32:36 -0300

    refactor layout component; remove LinkNav component and wrap Header in Suspense

--- app/(principal)/layout.jsx
+++ app/(principal)/layout.jsx
@@ -1,9 +1,12 @@
 import Header from "@/components/(header)/header";
+import { Suspense } from "react";
 
 export default async function CategoryLayout({ children }) {
   return (
     <main className="bg-gray-900 relative text-gray-100 min-h-[100svh] flex flex-col justify-between lg:justify-stretch w-[100svw] min-w-full lg:grid max-w-[2000px] mx-auto">
-      <Header />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Header />
+      </Suspense>
 
       <div className="max-w-full mx-auto p-4">{children}</div>
 
