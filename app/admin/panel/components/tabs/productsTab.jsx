e8a37e78dc8ceb15e994f204a1069254823420c0
lukkas98 <lucaspalma988@gmail.com>
2025-07-26 19:37:07 -0300

    fix: adjust layout and button styles in ProductsTab and Paginate components

--- app/admin/panel/components/tabs/productsTab.jsx
+++ app/admin/panel/components/tabs/productsTab.jsx
@@ -7,7 +7,7 @@ export default async function ProductsTab({ data }) {
   const totalPages = data.totalPages;
 
   return (
-    <div className="grid gap-4 mb-13">
+    <div className="mb-17 grid gap-4">
       <Paginate totalPages={totalPages} />
 
       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
