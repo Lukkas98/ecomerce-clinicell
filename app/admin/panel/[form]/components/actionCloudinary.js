8b126d762e87bfc7e399b536172c534578d48759
Lukkas98 <lucaspalma988@gmail.com>
2025-11-12 16:36:59 -0300

    refactor(actionCloudinary): comment out unused upload function

--- app/admin/panel/[form]/components/actionCloudinary.js
+++ app/admin/panel/[form]/components/actionCloudinary.js
@@ -3,36 +3,36 @@ import { verifySession } from "@/lib/auth";
 import { cloudinaryUploader } from "@/lib/cloudinary-config";
 import { revalidatePath } from "next/cache";
 
-export const uploadToCloudinary = async (
-  base64Image,
-  index,
-  name,
-  category
-) => {
-  try {
-    if (!(await verifySession()))
-      throw new Error("No tienes permisos para subir imagenes");
+// export const uploadToCloudinary = async (
+//   base64Image,
+//   index,
+//   name,
+//   category
+// ) => {
+//   try {
+//     if (!(await verifySession()))
+//       throw new Error("No tienes permisos para subir imagenes");
 
-    const capName = name.charAt(0).toUpperCase() + name.slice(1);
-    const capNameTrimmed = capName.trim();
-    const capCategory = category.charAt(0).toUpperCase() + category.slice(1);
-    const fileName = `${capNameTrimmed}-${index}`;
+//     const capName = name.charAt(0).toUpperCase() + name.slice(1);
+//     const capNameTrimmed = capName.trim();
+//     const capCategory = category.charAt(0).toUpperCase() + category.slice(1);
+//     const fileName = `${capNameTrimmed}-${index}`;
 
-    const result = await cloudinaryUploader.upload(base64Image, {
-      public_id: fileName,
-      format: "webp",
-      transformation: [{ quality: "auto", fetch_format: "webp" }],
-      overwrite: true,
-      folder: `${capCategory.trim()}/${capNameTrimmed}`,
-    });
+//     const result = await cloudinaryUploader.upload(base64Image, {
+//       public_id: fileName,
+//       format: "webp",
+//       transformation: [{ quality: "auto", fetch_format: "webp" }],
+//       overwrite: true,
+//       folder: `${capCategory.trim()}/${capNameTrimmed}`,
+//     });
 
-    return { url: result.secure_url, publicId: result.public_id };
-  } catch (error) {
-    throw new Error(`Error subiendo imagen: ${error.message}`);
-  } finally {
-    revalidatePath("/home", "page");
-  }
-};
+//     return { url: result.secure_url, publicId: result.public_id };
+//   } catch (error) {
+//     throw new Error(`Error subiendo imagen: ${error.message}`);
+//   } finally {
+//     revalidatePath("/home", "page");
+//   }
+// };
 
 export const deleteFromCloudinary = async (publicId) => {
   try {
